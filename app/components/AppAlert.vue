e2dd7692fd10c160f824a0ccc59ecb031d44bddc
Fantilong <2554509132@qq.com>
2026-08-30 23:13:40 +0800

    feat: 根据变量名获取，会被vite加工的静态图片资源，需要显示的使用import语法来将资源加载出来

--- app/components/AppAlert.vue
+++ app/components/AppAlert.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="app-alert">
         <slot></slot>
+        <img :src="imageUrl" alt="">
+
     </div>
 </template>
 
 <script setup lang="ts">
+const props = defineProps<{name: string}>()
+
+const imageUrl = computed(() => {
+    return `/img/${props.name}.png`
+})
 
 </script>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,36 @@
 <template>
    <div class="h1">helloe nuxt</div>
-    <AppAlert>his is an auto-imported component.</AppAlert>
+    <AppAlert name="画板备份 9">his is an auto-imported component.</AppAlert>
+    <!-- <img src="/img/画板备份 9.png" alt=""> -->
+    <!-- <img src="~/assets/img/画板备份 8.png" alt="" srcset=""> -->
+     <img :src="imgUrl" alt="" srcset="">
 </template>
 
 <script lang="ts" setup>
 // const runtimeConfig = useRuntimeConfig()
 
 // console.log('runtimeConfig', runtimeConfig);
+
+const light = () => import('./assets/img/画板备份 8.png?url')
+
+const images = import.meta.glob<string>('../assets/img/*.{png,jpg,svg}', {
+  query: '?url',
+  import: 'default',
+})
+
+
+async function getImageUrl (name: string) {
+  const load = images[`../assets/img/${name}.png`]
+
+  if (!load) {
+    throw new Error(`Unknown image: ${name}`)
+  }
+
+  return await load()
+}
+
+const imgUrl = (await getImageUrl('画板备份 8'))
+
 
 
 
